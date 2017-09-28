bf13010ba4ffcfd6275deccb9bd3139f88c6496e
xli76 <xli76@ncsu.edu>
2017-09-28 11:06:50 -0400

    fix confirm twice

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -72,7 +72,7 @@ class CarsController < ApplicationController
   # POST /cars.json
   def create
     @car = Car.new(car_params)
-
+    @car.status = "Available"
     respond_to do |format|
       if @car.save
         format.html {redirect_to @car, notice: 'Car was successfully created.'}
