164ccccfe200d9b7878d839b8077220edcf6bc06
xli76 <xli76@ncsu.edu>
2017-10-03 18:58:22 -0400

    add lpn limitation

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,19 +2,8 @@ class Car < ApplicationRecord
   # has_many :reservations
   mount_uploader :img_attach, CarImageUploader
   validates :lpn, :manufactuere, :model, :hrr, :style, :location, presence: true
-  validates :lpn, uniqueness: true
+  validates :lpn, uniqueness: true, numericality: { only_integer: true }, length: { is: 7 }
   validates :hrr, numericality: {greater_than: 0.0}
   validates :status, inclusion: {in: ['Reserved', 'Checkout', 'Available']}
 
-
-  def reserve lpn
-    @lpn = lpn
-    @car = Car.find(:lpn)
-    if @car.status == "available"
-      @car.status = "reserved"
-      true
-    else
-      false
-    end
-  end
 end
