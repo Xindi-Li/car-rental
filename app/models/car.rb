42c1ed65a8bbe5e4938c6ae5463b4a5460ccbda8
xli76 <xli76@ncsu.edu>
2017-10-08 18:42:45 -0400

    lpn limitation change

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car < ApplicationRecord
   # has_many :reservations
   mount_uploader :img_attach, CarImageUploader
   validates :lpn, :manufactuere, :model, :hrr, :style, :location, presence: true
-  validates :lpn, uniqueness: true, numericality: { only_integer: true }, length: { is: 7 }
+  validates :lpn, uniqueness: true, length: { is: 7 }
   validates :hrr, numericality: {greater_than: 0.0}
   validates :status, inclusion: {in: ['Reserved', 'Checkout', 'Available']}
   validates :style, inclusion: {in: ['Coupe', 'Sedan', 'SUV']}
